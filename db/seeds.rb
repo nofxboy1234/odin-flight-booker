17ff2c72ad8ba31dd1b8144f46ec2a93ffcf40ab
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-11 15:41:46 +0200

    Seed the database with default test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Airport.delete_all
+3.times do |i|
+  Airport.create(id: i,
+                 code: "Airport #{i}")
+end
+
+Booking.delete_all
+2.times do |i|
+  Booking.create(id: i)
+end
+
+Flight.delete_all
+2.times do |i|
+  Flight.create(id: i,
+                date: Date.today + i,
+                number_of_passengers: i + 1,
+                departure_airport_id: i,
+                arrival_airport_id: i + 1)
+end
+
+Passenger.delete_all
+2.times do |i|
+  Passenger.create(id: i,
+                   name: "Passenger #{i}",
+                   email: "passenger#{i}@palmboom.com",
+                  booking_id: i,
+                  flight_id: i)
+end
