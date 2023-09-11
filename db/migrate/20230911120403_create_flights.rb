63937b394f453cd73d51e0c937f8cb3f3de0a9a1
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-11 15:10:30 +0200

    Remove foreign_key constraint from :flights migration and add foreign key manually

--- db/migrate/20230911120403_create_flights.rb
+++ db/migrate/20230911120403_create_flights.rb
@@ -3,11 +3,13 @@ class CreateFlights < ActiveRecord::Migration[7.0]
     create_table :flights do |t|
       t.datetime :date
       t.integer :number_of_passengers
-      t.belongs_to :departure_airport, null: false, foreign_key: true
-      t.belongs_to :arrival_airport, null: false, foreign_key: true
+      t.belongs_to :departure_airport, null: false, foreign_key: false
+      t.belongs_to :arrival_airport, null: false, foreign_key: false
       t.belongs_to :booking, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_foreign_key :flights, :airports, column: :departure_airport_id
+    add_foreign_key :flights, :airports, column: :arrival_airport_id
   end
 end
