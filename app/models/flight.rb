fd02365ecc98d5994d2a77af0a2ce56123a37983
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-12 10:15:34 +0200

    Update :dates_with_flights scope on Flight model and update collection_select to put :date value in params

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,5 @@
 class Flight < ApplicationRecord
-  scope :dates_with_flights, -> { Flight.select(:date) }
+  scope :dates_with_flights, -> { Flight.select(:id, :date).order(:date) }
   
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
