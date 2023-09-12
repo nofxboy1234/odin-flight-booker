5d99a366de92cd90a11821f94bec16eeebf419a7
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-12 09:46:27 +0200

    Add :dates_with_flights scope to Flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
+  scope :dates_with_flights, -> { Flight.select(:date) }
+  
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
   # belongs_to :booking
