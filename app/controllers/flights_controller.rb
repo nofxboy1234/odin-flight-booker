e27ea4bafe87c6ad30754260bfb32d22924de7a9
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-12 12:19:01 +0200

    Add pry-byebug to Gemfile and update flights#index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,8 @@ class FlightsController < ApplicationController
 
   # GET /flights or /flights.json
   def index
-    @flights = Flight.all
+    binding.pry
+    # @flights = Flight.all
   end
 
   # GET /flights/1 or /flights/1.json
