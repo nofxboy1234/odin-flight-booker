dca96d066a4b5e3eb71bf2a3765973fef5924e31
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-13 17:03:08 +0200

    Change :reject_if option lambda in Booking model to a one line block

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,4 @@
 class Booking < ApplicationRecord
   has_many :passengers
-  accepts_nested_attributes_for :passengers, allow_destroy: true, reject_if: lambda do |attributes|
-    attributes[:name].blank? || attributes[:email].blank?
-  end
+  accepts_nested_attributes_for :passengers, allow_destroy: true, reject_if: lambda { |attributes| attributes[:name].blank? || attributes[:email].blank? }
 end
