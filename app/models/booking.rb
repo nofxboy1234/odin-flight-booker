1f502814ec124cffec5cce31cbea75d5821d1d3e
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-14 09:03:15 +0200

    Update models so Flight has_many :bookings instead of :passengers

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   has_many :passengers
   accepts_nested_attributes_for :passengers, allow_destroy: true, reject_if: lambda { |attributes| attributes[:name].blank? || attributes[:email].blank? }
-  
+  belongs_to :flight
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -9,7 +9,7 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
 
-  has_many :passengers
+  has_many :bookings
 
   def date_formatted
     date.strftime('%d/%m/%Y')
