6310f62ec57fbaed8bb2e57daf1b16c6ac1db3ad
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-14 08:58:29 +0200

    Remove commented code

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   has_many :passengers
   accepts_nested_attributes_for :passengers, allow_destroy: true, reject_if: lambda { |attributes| attributes[:name].blank? || attributes[:email].blank? }
+  
 end
