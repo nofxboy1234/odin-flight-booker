4ed27836dbfeec19343d9f739044f17720dc8605
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-01 14:15:53 +0200

    Prevent removing last passenger on a booking

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -1,47 +1,55 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="passenger"
 export default class extends Controller {
-  static targets = ['passengerList', 'passengerItem']
+  static targets = ['passengerList', 'passengerItem'];
 
   connect() {
-    console.log(this.passengerListTarget)
-    console.log(this.passengerItemTarget)
+    console.log(this.passengerListTarget);
+    console.log(this.passengerItemTarget);
   }
 
   addPassenger() {
-    console.log('Add Passenger')
+    console.log('Add Passenger');
 
-    const clone = this.passengerItemTarget.content.cloneNode(true)
+    const clone = this.passengerItemTarget.content.cloneNode(true);
     // console.log(this.passengerListTarget.getElementsByTagName('li'))
-    const passengerIndex = this.passengerListTarget.getElementsByTagName('li').length
+    const passengerIndex =
+      this.passengerListTarget.getElementsByTagName('li').length;
     // const passengerIndex = clone.firstElementChild.children.length / 2
 
-    let id = `booking_passengers_attributes_${passengerIndex}_name`
-    let idPieces = id.split('_')
-    let name = `${idPieces[0]}[${idPieces[1]}_${idPieces[2]}][${idPieces[3]}][${idPieces[4]}]`
+    let id = `booking_passengers_attributes_${passengerIndex}_name`;
+    let idPieces = id.split('_');
+    let name = `${idPieces[0]}[${idPieces[1]}_${idPieces[2]}][${idPieces[3]}][${idPieces[4]}]`;
 
-    clone.firstElementChild.children[0].setAttribute('for', id)
-    clone.firstElementChild.children[1].setAttribute('id', id)
-    clone.firstElementChild.children[1].setAttribute('name', name)
+    clone.firstElementChild.children[0].setAttribute('for', id);
+    clone.firstElementChild.children[1].setAttribute('id', id);
+    clone.firstElementChild.children[1].setAttribute('name', name);
 
-    id = `booking_passengers_attributes_${passengerIndex}_email`
-    idPieces = id.split('_')
-    name = `${idPieces[0]}[${idPieces[1]}_${idPieces[2]}][${idPieces[3]}][${idPieces[4]}]`
+    id = `booking_passengers_attributes_${passengerIndex}_email`;
+    idPieces = id.split('_');
+    name = `${idPieces[0]}[${idPieces[1]}_${idPieces[2]}][${idPieces[3]}][${idPieces[4]}]`;
 
-    clone.firstElementChild.children[2].setAttribute('for', id)
-    clone.firstElementChild.children[3].setAttribute('id', id)
-    clone.firstElementChild.children[3].setAttribute('name', name)
+    clone.firstElementChild.children[2].setAttribute('for', id);
+    clone.firstElementChild.children[3].setAttribute('id', id);
+    clone.firstElementChild.children[3].setAttribute('name', name);
 
-    this.passengerListTarget.appendChild(clone)
+    this.passengerListTarget.appendChild(clone);
   }
 
   removePassenger(event) {
-    console.log('removePassenger')
+    console.log('removePassenger');
 
-    const element = event.target
-    console.log(element)
-    console.log(element.parentElement)
-    element.parentElement.remove()
+    const number_of_passengers =
+      this.passengerListTarget.getElementsByTagName('li').length;
+    if (number_of_passengers === 1) {
+      console.log('Only 1 passenger left. Cannot remove them!')
+      return;
+    }
+
+    const element = event.target;
+    console.log(element);
+    console.log(element.parentElement);
+    element.parentElement.remove();
   }
 }
