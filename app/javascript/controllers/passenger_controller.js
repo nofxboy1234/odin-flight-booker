afda07ba8f9d176d52c7c972dd9e50bb63e2532e
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-01 13:38:43 +0200

    Set id and name attributes on template content

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -13,6 +13,26 @@ export default class extends Controller {
     console.log('Add Passenger')
 
     const clone = this.passengerItemTarget.content.cloneNode(true)
+    // console.log(this.passengerListTarget.getElementsByTagName('li'))
+    const passengerIndex = this.passengerListTarget.getElementsByTagName('li').length
+    // const passengerIndex = clone.firstElementChild.children.length / 2
+
+    let id = `booking_passengers_attributes_${passengerIndex}_name`
+    let idPieces = id.split('_')
+    let name = `${idPieces[0]}[${idPieces[1]}_${idPieces[2]}][${idPieces[3]}][${idPieces[4]}]`
+
+    clone.firstElementChild.children[0].setAttribute('for', id)
+    clone.firstElementChild.children[1].setAttribute('id', id)
+    clone.firstElementChild.children[1].setAttribute('name', name)
+
+    id = `booking_passengers_attributes_${passengerIndex}_email`
+    idPieces = id.split('_')
+    name = `${idPieces[0]}[${idPieces[1]}_${idPieces[2]}][${idPieces[3]}][${idPieces[4]}]`
+
+    clone.firstElementChild.children[2].setAttribute('for', id)
+    clone.firstElementChild.children[3].setAttribute('id', id)
+    clone.firstElementChild.children[3].setAttribute('name', name)
+
     this.passengerListTarget.appendChild(clone)
   }
 }
