74b0c788674bdc66604af09271040a050bd9bda1
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-01 11:41:25 +0200

    Add passengerList target

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="passenger"
 export default class extends Controller {
-  static targets = ['passengerItem']
+  static targets = ['passengerList', 'passengerItem']
 
   connect() {
+    console.log(this.passengerListTarget)
     console.log(this.passengerItemTarget)
   }
 }
